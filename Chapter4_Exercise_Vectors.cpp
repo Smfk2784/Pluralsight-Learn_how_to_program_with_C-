69f93668f3e64646824969ff509b5fba9b7e8707
Florian Kunkel <Florian.Kunkel@hotmail.de>
2019-10-27 20:58:55 +0100

    Add chapter 4 exercise Demo: vector

---
+++ Chapter4_Exercise_Vectors.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> // for sort and count
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Declare vector of type integer
+    vector<int> vi;
+
+    // Member function off class vector --> Put item to end of the vector
+    for (int i = 0; i < 10; i++)
+    {
+        vi.push_back(i);
+    }
+
+    // Range based "for", for every element in the vector, do this
+    for (auto item:vi)
+    {
+        cout << item << " ";
+    }
+
+    vector<string> vs;
+
+    cout << "enter three words ";
+    for (int i = 0; i < 3; i++)
+    {
+        string s;
+        cin >> s;
+        vs.push_back(s);
+    } 
+
+    for (auto item:vs)
+    {
+        cout << item << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
